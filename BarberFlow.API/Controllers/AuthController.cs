47cc7326c60917d4668e50dee10decb4e0c43c5c
Henrique Brum Riva <henrique.riva@farroupilha.local>
2026-03-13 21:32:31 -0300

    feat: add controller auth and user

---
+++ BarberFlow.API/Controllers/AuthController.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BarberFlow.API.Controllers;
+
+[ApiController]
+[Route("barber/auth")]
+public class AuthController : ControllerBase
+{
+}
---
+++ BarberFlow.API/Controllers/UserController.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BarberFlow.API.Controllers;
+
+[ApiController]
+[Route("barber/user")]
+public class UserController : ControllerBase
+{
+}
