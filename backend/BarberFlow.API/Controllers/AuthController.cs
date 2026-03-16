cb621ba093f2ffc4139a5122debcdd8d23901075
Henrique Riva <hbriva123@gmail.com>
2026-03-15 22:11:20 -0300

    feat: initial implement auth controller

--- backend/BarberFlow.API/Controllers/AuthController.cs
+++ backend/BarberFlow.API/Controllers/AuthController.cs
@@ -4,6 +4,28 @@ namespace BarberFlow.API.Controllers;
 
 [ApiController]
 [Route("barber/auth")]
-public class AuthController : ControllerBase
+public class AuthController(IAuthService authService) : ControllerBase
 {
+    private readonly IAuthService _authService = authService;
+
+    [Route("login")]
+    [HttpPost]
+    public async Task<IActionResult> Login(LoginCommand loginCommand)
+    {
+        return Ok();
+    }
+
+    [Route("register")]
+    [HttpPost]
+    public async Task<IActionResult> Register(RegisterCommand registerCommand)
+    {
+        return Ok();
+    }
+
+    [Route("logout")]
+    [HttpPost]
+    public async Task<IActionResult> Logout(LogoutCommand logoutCommand)
+    {
+        return Ok();
+    }
 }
