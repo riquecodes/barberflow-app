491f8d40d93786a0b9381a621ba0aaecd1be53a6
Henrique Brum Riva <henrique.riva@farroupilha.local>
2026-03-20 21:31:48 -0300

    feat: add domains

---
+++ backend/BarberFlow.Domain/ServicesItems/Services.cs
@@ -0,0 +1,14 @@
+﻿namespace BarberFlow.Domain.ServicesItems;
+
+public class Services
+{
+    public int ServiceId { get; set; }
+
+    public string? Name { get; set; }
+
+    public decimal Price { get; set; }
+
+    public string? Description { get; set; }
+
+    public TimeOnly Duration { get; set; }
+}
--- backend/BarberFlow.Domain/UserItems/User.cs
+++ backend/BarberFlow.Domain/UserItems/User.cs
@@ -1,9 +1,17 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿namespace BarberFlow.Domain.UserItems;
 
-namespace BarberFlow.Domain.LoginItems;
-
-internal class User
+public class User
 {
+    public int UserId { get; set; }
+
+    public string? Name { get; set; }
+
+    public string? Cpf { get; set; }
+
+    public string? Phone { get; set; }
+
+    public string? Email { get; set; }
+
+    public string? Password { get; set; }
+    
 }
