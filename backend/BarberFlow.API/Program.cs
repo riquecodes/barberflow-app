7bfa8a7c20fc108a8aa44a4b44381dd339f6a9a1
Henrique Riva <hbriva123@gmail.com>
2026-04-11 09:09:28 -0300

    mega fix: move folders to .API

--- backend/BarberFlow.API/Program.cs
+++ backend/BarberFlow.API/Program.cs
@@ -135,11 +135,15 @@ builder.Services.AddCors(options =>
 
 var app = builder.Build();
 
-app.UseSwagger();
-app.UseSwaggerUI(c =>
+if (app.Environment.IsDevelopment())
 {
-    c.SwaggerEndpoint("/swagger/v1/swagger.json", "BarberFlow API v1");
-});
+    app.UseSwagger();
+    app.UseSwaggerUI(c =>
+    {
+        c.SwaggerEndpoint("/swagger/v1/swagger.json", "BarberFlow API v1");
+        c.RoutePrefix = string.Empty;
+    });
+}
 
 app.UseCors("AllowAll"); 
 
